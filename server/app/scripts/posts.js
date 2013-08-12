0773cf68a3c393c6cec97c22667b5496b005b6a6
Michael Klein <klein@monbro.de>
2013-08-12 22:01:32 +0100

    easier and cheaper click counter...

--- server/app/scripts/posts.js
+++ server/app/scripts/posts.js
@@ -20,19 +20,6 @@ module.exports = function(app, collectionPosts, collectionTags, collectionNotifi
         });
     });
 
-    // Increase click counter for a specific video
-    app.post('/counter-post', function(req, res) {
-        var BSON = mongodb.BSONPure;
-        var data = {
-            id: req.body.id,
-            oid: new BSON.ObjectID(req.body.id)
-        };
-        // update post by raising click counter
-        collectionPosts.update({'_id': data.oid }, {$inc: {click_counter: 1}}, function(){
-            res.json('ok');
-        });
-    });
-
     // Create Post and save into db
     app.post('/post', auth.isAuth, function(req, res) {
         collectionPosts.insert(req.body, function(err, docs) {
@@ -67,6 +54,12 @@ module.exports = function(app, collectionPosts, collectionTags, collectionNotifi
         if(req.params.id.length === 24){
             var BSON = mongodb.BSONPure;
             var oId = new BSON.ObjectID(req.params.id);
+
+            // raise counter
+            collectionPosts.update({'_id': oId }, {$inc: {click_counter: 1}}, function(){
+                // nothing to do
+            });
+
             collectionPosts.find({'_id': oId }).toArray(function(err, results){
                 collectionTags.find({ tagName: { $in: results[0].tags } }).toArray(function(err, tag_results){
                     results[0].tags = tag_results;
